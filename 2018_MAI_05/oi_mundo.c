2abefb64d3a1ae8b452b026e163868317edd11a2
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2018-05-04 09:17:30 -0300

    Primeira Aula

---
+++ 2018_MAI_05/oi_mundo.c
@@ -0,0 +1,15 @@
+/*
+ * Oi mundo com a linguagem C
+ *
+ * Prof. Me. Orlando Saraiva Jr
+ * Criado em : 03/05/2018
+ */
+
+ #include <stdlib.h>
+ #include <stdio.h>
+
+
+int main(void) {
+	printf("Oi mundo \n");
+	return 0;
+}
---
+++ 2018_MAI_05/oi_mundo.cpp
@@ -0,0 +1,15 @@
+/*
+ * Oi mundo com a linguagem C
+ *
+ * Prof. Me. Orlando Saraiva Jr
+ * Criado em : 03/05/2018
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+int main(void) {
+	cout << "Oi mundo " << endl;
+	return 0;
+}
